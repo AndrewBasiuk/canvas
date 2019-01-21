6976693586db55029bec13fe7244b91c59559c25
Andrew Basiuk <andrii.basiuk@mgid.com>
2019-01-21 18:48:46 +0200

    added circleMotion.js

--- circleMotion.js
+++ circleMotion.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let pos = {};
-let num = 1000;
+let num = 1;
 let allCircles = [];
 let colorArr = ["#0d0f11", "#61000d", "#d2374a", "#5d9700", "#092a3a"];
 
@@ -14,7 +14,7 @@ function mousePos(e) {
     pos.x = e.pageX - e.target.getBoundingClientRect().left;
 }
 
-canvas.addEventListener("mousemove", function(e){
+canvas.addEventListener("mousemove", function (e) {
     mousePos(e);
 });
 
@@ -24,9 +24,11 @@ function Circle(x, y, dx, dy, rad, color) {
     this.dx = dx;
     this.dy = dy;
     this.rad = rad;
-    this.color = color;
+    // this.color = color;
+    this.radians = color;
+    this.velocity = 0.02;
 
-    this.draw = function() {
+    this.draw = function () {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.rad, 0, 2 * Math.PI, true);
         ctx.fillStyle = this.color;
@@ -34,23 +36,11 @@ function Circle(x, y, dx, dy, rad, color) {
         ctx.closePath();
     }
 
-    this.update = function() {
-        if (this.x > canvas.width - this.rad || this.x < this.rad) {
-            this.dx = -this.dx;
-        }
-        if (this.y > canvas.height - this.rad || this.y < this.rad) {
-            this.dy = -this.dy;
-        }
-        this.x += this.dx;
-        this.y += this.dy;
+    this.update = function () {
+        this.radians += this.velocity;
+        this.x = x + Math.cos(this.radians)*100;
+        this.y = y + Math.sin(this.radians)*100;
 
-        if (pos.x - 50 < this.x && pos.x + 50 > this.x && pos.y - 50 < this.y && pos.y + 50 > this.y) {
-            if(this.rad <= 30) {
-                this.rad += 5;
-            }
-        } else if(this.rad >= rad) {
-            this.rad -= 1;
-        }
 
         this.draw();
     }
@@ -59,25 +49,38 @@ function Circle(x, y, dx, dy, rad, color) {
 //set random parameter for each objekt
 for (let index = 0; index < num; index++) {
     let rad = getRandomInt(3, 15);
-    let x = getRandomInt(rad, 1000-rad);
-    let y = getRandomInt(rad, 600-rad);
+    let x = getRandomInt(rad, 1000 - rad);
+    let y = getRandomInt(rad, 600 - rad);
     let dx = getRandomInt(-1, 1);
     let dy = getRandomInt(-1, 1);
     let color = colorArr[getRandomInt(0, 4)];
 
-    if(dx == 0 && dy == 0) {
+    if (dx == 0 && dy == 0) {
         dx = 1;
         dy = -1;
     }
-    allCircles.push(new Circle(x, y, dx, dy, rad, color));    
+    allCircles.push(new Circle(x, y, dx, dy, rad, color));
 }
+
+var circle = new Circle(500, 300, 1, 2, 15, 2*Math.PI);
+var circle2 = new Circle(500, 300, 1, 2, 15, 3*Math.PI/2);
+var circle3 = new Circle(500, 300, 1, 2, 15, Math.PI);
+var circle4 = new Circle(500, 300, 1, 2, 15, Math.PI/2);
+
 
 function animation() {
     requestAnimationFrame(animation);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    allCircles.forEach(function(el) {
-        el.update();
-    });
+    circle.update();
+    circle2.update();
+    circle3.update();
+    circle4.update();
+
+
+
+    // allCircles.forEach(function (el) {
+    //     el.update();
+    // });
 }
 animation();
